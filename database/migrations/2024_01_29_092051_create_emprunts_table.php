37b2cb4d8411a11a1c56c1a4bc13456ba404d878
anasmak04 <anasdev8@gmail.com>
2024-01-29 10:27:22 +0100

    added migrations tables livres emprunts

---
+++ database/migrations/2024_01_29_092051_create_emprunts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('emprunts', function (Blueprint $table) {
+            $table->id();
+            $table->string("description");
+            $table->date("reservation_date");
+            $table->date("return_date");
+            $table->boolean("is_returned");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('emprunts');
+    }
+};
